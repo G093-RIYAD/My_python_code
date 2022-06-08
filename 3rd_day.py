69405658424137a340d4701c75836ed5986c4af3
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-08 15:58:34 +0600

    Create 3rd_day.py

---
+++ 3rd_day.py
@@ -0,0 +1,14 @@
+def student_identity(name, ID, payment, section, advisor):
+    print("your name is : " + name )
+    print("your ID is : " + ID   )
+    print(  " your payment stutus : " + payment )
+    print(  "your section is : " +  section)
+    print( "your advisor : " + advisor)
+
+student_identity("riyad", "221902093", "paid", "DH", "MD mahbubur Rahman")
+student_identity("ria", "221902325", "paid", "DH", "MD mahbubur Rahman")
+student_identity("nafiul", "221902348", "paid", "DH", "MD mahbubur Rahman")
+student_identity("rifat", "221902333", "paid", "DH", "MD mahbubur Rahman")
+student_identity("sumaiya", "221902352", "paid", "DH", "MD mahbubur Rahman")
+student_identity("shihab", "221902329", "paid", "DH", "MD mahbubur Rahman")
+student_identity("nadia", "221902326", "paid", "DH", "MD mahbubur Rahman")
