c40c13121a4e3273f2abf4f8e41549c4dc5f28fb
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-08 15:53:43 +0600

    Create 2nd_day.py

---
+++ 2nd_day.py
@@ -0,0 +1,22 @@
+first_number = 69
+second_number = 96
+add = first_number + second_number
+sub = second_number - first_number
+mul = first_number * 99
+div = second_number / 10
+div_int = second_number // 10
+div_rem = second_number % 10
+
+print(f"add : {add}")
+print(f"sub : {sub}")
+print(f"mul : {mul}")
+print(f"div : {div}")
+print(f"int : {div_int}")
+print(f"rem : {div_rem}")
+
+name = "REFAT"
+value = 69
+store_type = "FASTFOOD"
+full_store_name = name+ str(value) +" "+ store_type
+print(full_store_name)
+print(f"Lenth of the foodshop name : {len(full_store_name)}")
