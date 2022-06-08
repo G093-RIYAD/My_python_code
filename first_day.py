07eac9d904aa0a6be418e56f5732f4fcab1e0af1
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-08 15:47:37 +0600

    Create first_day.py

---
+++ first_day.py
@@ -0,0 +1,10 @@
+print("hellow world")
+print(1,2,3)
+
+def numbers(first, second):
+    return first * second / 5
+
+num01= 55
+num02= 60
+refat = numbers(num01, num02)
+print(f"{num01}*{num02}={refat}")
