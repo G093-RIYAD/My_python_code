849750528f1cf0fa35905068f0957710b0ea9405
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-12 20:52:40 +0600

    Update 6th_day.py

--- 6th_day.py
+++ 6th_day.py
@@ -23,3 +23,36 @@ number = int(input("Give a number:"))
 the_user_number = number>= 80
 pmassage = "The number is greater than or equals 80: " + str(the_user_number)
 print(massage)
+
+#Python Boolean
+
+print(10>9)
+print(299>3999)
+print(899==90)
+
+#Python Boolean with true value;
+
+print(bool("yo What's up?"))
+print(bool(79))
+print(bool(["apple", "cherry", "banana"]))
+
+#Python Boolean false value;
+
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+
+
+a = 980
+b = 60
+
+if b > a:
+    print("b is grater than a")
+else:
+    print("b isn't grater than a")
+
+
