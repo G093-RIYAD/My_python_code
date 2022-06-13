9615aed0cc1300da737c06a0b97df1701e4e4dbd
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-13 20:11:57 +0600

    Create 7th_day.py

---
+++ 7th_day.py
@@ -0,0 +1,15 @@
+def even_num(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
+even_numbers = []
+starting = 0
+user_input = int(input("Limit : "))
+while starting <= user_input:
+    if even_num(starting):
+        even_numbers.append(starting)
+    starting = starting + 1
+print(f"Even numbers : {even_numbers}")
+print("Finished")
