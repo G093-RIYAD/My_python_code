f91c36f07f30e0145e1ddace89ab316c905708d5
MD. Nobiul Haque Riyad <103303700+X86JP-Riyad@users.noreply.github.com>
2022-06-13 20:59:52 +0600

    Update 7th_day.py

--- 7th_day.py
+++ 7th_day.py
@@ -15,7 +15,14 @@ while i < 6:
   if i == 3:
     continue
   print(i)
-      
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
 starting = 0
 while starting < 1000:
     if even_num(starting):
